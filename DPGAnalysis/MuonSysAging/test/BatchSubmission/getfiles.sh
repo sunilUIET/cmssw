b40678ff0585a29bd450b8646108d8527be422e2
sunil.bansal <subansal@cern.ch>
2016-10-19 17:21:54 +0200

    further cleaning

--- DPGAnalysis/MuonSysAging/test/BatchSubmission/getfiles.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/tcsh
-
-set dirin = $*
-foreach i (`ls -1 "$dirin" | grep .root | awk '{print $NF}'`)
-
-echo "'file:"$dirin$i"',"
-
-end
-
--- DPGAnalysis/MuonSysAging/test/BatchSubmission/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/csh
-
-set i = 0
-while ($i<700)
-./script.csh $i
-@ i++
-end
-
-#exit 0
