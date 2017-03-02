ffe9f3aea683c26ee21897d627427baa2ce7a5dc
sunil.bansal <subansal@cern.ch>
2017-03-02 17:24:38 +0100

    CMSSW81XPre16

--- Configuration/Eras/python/Era_Run2_2017_trackingRun2_cff.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from Configuration.Eras.ModifierChain_run2_2017_core_cff import run2_2017_core
-
-Run2_2017_trackingRun2 = cms.ModifierChain(run2_2017_core)
-
--- Validation/HGCalValidation/python/test/hgcBHValidation_cfi.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-hgcalBHValidation = cms.EDAnalyzer('HGCalBHValidation',
-                                   ModuleLabel   = cms.untracked.string('g4SimHits'),
-                                   HitCollection = cms.untracked.string('HcalHits'),
-                                   DigiCollection= cms.untracked.InputTag('simHcalUnsuppressedDigis','HBHEUpgradeDigiCollection'),
-                                   Sample        = cms.untracked.int32(5),
-                                   Threshold     = cms.untracked.double(15.0)
-                                   )
